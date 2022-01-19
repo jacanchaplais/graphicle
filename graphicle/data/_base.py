00bedce4ffe56bbb04b10eb0bb45c74c103ab7c6
Jacan Chaplais <jlc1n20@soton.ac.uk>
2022-01-19 11:11:52 +0000

    added foundational data structures

--- graphicle/data/_base.py
+++ graphicle/data/_base.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
 
 
+class ArrayBase(ABC):
+    @property
+    @abstractmethod
+    def data(self):
+        pass
+
 class ParticleBase(ABC):
     @property
     @abstractmethod
@@ -22,59 +28,48 @@ class ParticleBase(ABC):
     def final(self):
         pass
 
-class NetworkBase(ABC):
+class EdgeBase(ABC):
+    @property
+    @abstractmethod
+    def data(self):
+        pass
+
     @property
     @abstractmethod
     def nodes(self):
         pass
 
+class GraphicleBase(ABC):
     @property
     @abstractmethod
     def edges(self):
         pass
 
-class ParticleNetworkBase(ABC):
+    # @abstractmethod
+    # def vertex_pdg(self):
+    #     pass
+
+    # @abstractmethod
+    # def to_networkx(self):
+    #     pass
+
+    # @abstractmethod
+    # def to_pandas(self):
+    #     pass
+
+    # @abstractmethod
+    # def signal_mask(self):
+    #     pass
+
+    # @abstractmethod
+    # def copy(self):
+    #     pass
+
+# ---------------------------
+# composite pattern for masks
+# ---------------------------
+class MaskBase(ABC):
     @property
     @abstractmethod
-    def edges(self):
-        pass
-
-    @property
-    @abstractmethod
-    def pmu(self):
-        pass
-
-    @property
-    @abstractmethod
-    def color(self):
-        pass
-
-    @property
-    @abstractmethod
-    def pdg(self):
-        pass
-
-    @property
-    @abstractmethod
-    def final(self):
-        pass
-
-    @abstractmethod
-    def vertex_pdg(self):
-        pass
-
-    @abstractmethod
-    def to_networkx(self):
-        pass
-
-    @abstractmethod
-    def to_pandas(self):
-        pass
-
-    @abstractmethod
-    def signal_mask(self):
-        pass
-
-    @abstractmethod
-    def copy(self):
+    def data(self):
         pass
