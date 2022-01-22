331fbe7a07e2f41751cb9705d6036868d95f5787
G.Cerro <gc2c20@cyan54.cluster.local>
2022-01-22 11:46:19 +0000

    drafts of switching edge/node and removing duplicates

--- graphicle/switch.py
+++ graphicle/switch.py
@@ -1,4 +1,4 @@
-class switch:
+class ntool:
     def switch_edgenode(G):
         import networkx as _nx
 
@@ -8,3 +8,23 @@ class switch:
         L = _nx.relabel.relabel_nodes(L, edge_dict)
 
         return L
+
+    def remove_duplicates(G):
+        # I find the duplicate as the nodes with in_degree=out_degree=1
+        duplicates = [
+            node
+            for node in G.nodes()
+            if G.in_degree(node) == G.out_degree(node) == 1
+        ]
+
+        for duplo in duplicates:
+            vertex_in = list(G.in_edges(duplo))[0][0]
+            vertex_out = list(G.out_edges(duplo))[0][1]
+
+            # remove the node and the edges
+            G.remove_node(duplo)
+
+            # add the new edge
+            G.add_edge(vertex_in, vertex_out)
+
+        return G
