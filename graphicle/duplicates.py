89c72fd8f71d0e3703de4b213bbda92407fa4db8
G.Cerro <giorgiocerro96@gmail.com>
2022-01-28 18:59:14 +0000

    #6 new method without networkx but only numpy

--- graphicle/duplicates.py
+++ graphicle/duplicates.py
@@ -1,38 +1,33 @@
 import numpy as np
 import networkx as nx
+import copy
 
 
-def duplicate_mask(graph):
-    # take the graphicle object and create the network
-    edge_dict = graph.adj.to_dicts()
-    nx_graph = nx.DiGraph(edge_dict["edges"])
-    # edge_dict = {edge: i for i, edge in enumerate(nx_graph.edges())}
-
-    # find the duplicates
-    duplicates = np.array(
-        [
-            list(nx_graph.in_edges(node))[0]
-            for node in nx_graph.nodes()
-            if nx_graph.in_degree(node) == nx_graph.out_degree(node) == 1
-        ],
-        dtype=[("in", "<i4"), ("out", "<i4")],
+def duplicate_mask2(graph):
+    graph_masked = copy.deepcopy(graph)
+    in_deg = np.unique(
+        graph_masked.adj.edges["in"], return_index=True, return_counts=True
+    )
+    out_deg = np.unique(
+        graph_masked.adj.edges["out"], return_index=True, return_counts=True
     )
 
-    # create a mask
-    # edge_dict = {edge: i for i, edge in enumerate(nx_graph.edges())}
-    mask = np.ones(graph.adj.edges.shape, bool)
-    indices = np.array(
-        [np.where(graph.adj.edges == item)[0] for item in duplicates]
-    )
-    mask[indices] = False
-
+    mask = np.ones(graph_masked.adj.edges.shape, bool)
+    duplicates = []
+    for idx in range(len(in_deg[0])):
+        if in_deg[2][idx] == 1:
+            _out_idx = np.where(out_deg[0] == in_deg[0][idx])[0]
+            if len(_out_idx) > 0 and out_deg[2][_out_idx] == 1:
+                duplicates.append(graph_masked.adj.edges[out_deg[1][idx]])
+                mask[out_deg[1][idx]] = False
+    duplicates = np.array(duplicates, dtype=[("in", "<i4"), ("out", "<i4")])
     # update the edges
     for duplo in duplicates:
-        # find and update the edge in the graph
-        edge_idx = np.where(graph.adj.edges["in"] == duplo[1])[0]
-        graph.adj.edges["in"][edge_idx] = duplo[0]
+        # find and update the edge in the graph_masked
+        edge_idx = np.where(graph_masked.adj.edges["in"] == duplo[1])[0]
+        graph_masked.adj.edges["in"][edge_idx] = duplo[0]
         # check if needed to update the duplicates list
         duplo_idx = np.where(duplicates["in"] == duplo[1])[0]
         if len(duplo_idx) > 0:
             duplicates["in"][duplo_idx] = duplo[0]
-    return graph[mask]
+    return graph_masked[mask]
