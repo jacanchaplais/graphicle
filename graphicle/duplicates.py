003a50cb722557d1f50d99796e01b990d73e7cc9
G.Cerro <giorgiocerro96@gmail.com>
2022-01-31 14:46:17 +0000

    #6 Removed a for loop and an if statement; gained 1ms per run

--- graphicle/duplicates.py
+++ graphicle/duplicates.py
@@ -3,7 +3,7 @@ import networkx as nx
 import copy
 
 
-def duplicate_mask2(graph):
+def duplicate_mask(graph):
     graph_masked = copy.deepcopy(graph)
     in_deg = np.unique(
         graph_masked.adj.edges["in"], return_index=True, return_counts=True
@@ -13,21 +13,16 @@ def duplicate_mask2(graph):
     )
 
     mask = np.ones(graph_masked.adj.edges.shape, bool)
-    duplicates = []
-    for idx in range(len(in_deg[0])):
-        if in_deg[2][idx] == 1:
-            _out_idx = np.where(out_deg[0] == in_deg[0][idx])[0]
-            if len(_out_idx) > 0 and out_deg[2][_out_idx] == 1:
-                duplicates.append(graph_masked.adj.edges[out_deg[1][idx]])
-                mask[out_deg[1][idx]] = False
-    duplicates = np.array(duplicates, dtype=[("in", "<i4"), ("out", "<i4")])
-    # update the edges
-    for duplo in duplicates:
-        # find and update the edge in the graph_masked
-        edge_idx = np.where(graph_masked.adj.edges["in"] == duplo[1])[0]
-        graph_masked.adj.edges["in"][edge_idx] = duplo[0]
-        # check if needed to update the duplicates list
-        duplo_idx = np.where(duplicates["in"] == duplo[1])[0]
-        if len(duplo_idx) > 0:
-            duplicates["in"][duplo_idx] = duplo[0]
+    # indices of in_deg == 1
+    in_deg_indices = np.where(in_deg[2] == 1)[0]
+    for idx in in_deg_indices:
+        _out_idx = np.where(out_deg[0] == in_deg[0][idx])[0]
+        # if out_deg is also ==1
+        if len(_out_idx) > 0 and out_deg[2][_out_idx] == 1:
+            mask[out_deg[1][idx]] = False
+            duplo = graph_masked.adj.edges[out_deg[1][idx]]
+
+            edge_idx = np.where(graph_masked.adj.edges["in"] == duplo[1])[0]
+            graph_masked.adj.edges["in"][edge_idx] = duplo[0]
+
     return graph_masked[mask]
