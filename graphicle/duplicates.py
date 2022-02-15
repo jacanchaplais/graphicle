28d49207b165c9121c9df0a89e3db3c60ada495c
Jacan Chaplais <jacan.chaplais@gmail.com>
2022-02-15 11:59:23 +0000

    debt: branchless, vectorised duplicate mask #16

--- graphicle/duplicates.py
+++ graphicle/duplicates.py
@@ -1,28 +1,74 @@
 import numpy as np
-import networkx as nx
-import copy
+import numpy.lib.recfunctions as rfn
+
+from typicle import Types
+
+from typing import TYPE_CHECKING
+import graphicle as gcl
 
 
-def duplicate_mask(graph):
-    graph_masked = copy.deepcopy(graph)
-    in_deg = np.unique(
-        graph_masked.adj.edges["in"], return_index=True, return_counts=True
+_types = Types()
+
+
+def duplicate_mask(adj: gcl.AdjacencyList) -> gcl.MaskArray:
+    """
+
+    Notes
+    -----
+    Duplicate particles in full event graphs are identified easily.
+    Two particles are duplicates if they enter and leave the same
+    interaction vertex, one-edge-in and one-edge-out.
+    This is because they physically are unchanged unless they radiate
+    or decay.
+
+    This function simply identifies vertices in particle-as-edge
+    generation graphs with in-degree and out-degree of one.
+
+    To do this, a set is constructed of the vertex ids which only turn
+    up once in the `in` and `out` fields of the edge list.
+    The intersection of the `in` and `out` sets is found, leaving only
+    vertices with both and in-degree and out-degree of one.
+    Finally, a mask is constructed highlighting the edges belonging to
+    those vertices.
+    """
+    edges = adj.edges
+    mask = np.zeros_like(edges, dtype=_types.bool)
+
+    def unique_array(direction: str):
+        """Returns a structured array of the unique vertices in the
+        edge list's given direction.
+        Fields are vertex id, array index, and frequency count.
+        """
+        unique_dtype = [("vtx", "<i4"), ("index", "<i4"), ("count", "<i4")]
+        unique_dtype = np.dtype(unique_dtype)  # type: ignore
+        return rfn.unstructured_to_structured(
+            np.dstack(  # join the output arrays of unique together
+                np.unique(
+                    edges[direction], return_index=True, return_counts=True
+                )
+            ).squeeze(),  # remove dimension nesting
+            unique_dtype,
+        )
+
+    # get indices and counts for each vertex id
+    in_unique = unique_array("in")
+    out_unique = unique_array("out")
+
+    # select where in and out degree of vertices is 1
+    def singles(unique):
+        """Returns a structured unique array, masked such that only
+        elements with a count of 1 remain.
+        """
+        mask = unique["count"] == 1
+        return unique[mask]
+
+    in_singles = singles(in_unique)
+    out_singles = singles(out_unique)
+    # find indices for which vertices have both in and out degrees of 1
+    dup_vtxs, in_dup_idxs, out_dup_idxs = np.intersect1d(
+        in_singles["vtx"], out_singles["vtx"], return_indices=True
     )
-    out_deg = np.unique(
-        graph_masked.adj.edges["out"], return_index=True, return_counts=True
-    )
-
-    mask = np.ones(graph_masked.adj.edges.shape, bool)
-    # indices of in_deg == 1
-    in_deg_indices = np.where(in_deg[2] == 1)[0]
-    for idx in in_deg_indices:
-        _out_idx = np.where(out_deg[0] == in_deg[0][idx])[0]
-        # if out_deg is also ==1
-        if len(_out_idx) > 0 and out_deg[2][_out_idx] == 1:
-            mask[out_deg[1][idx]] = False
-            duplo = graph_masked.adj.edges[out_deg[1][idx]]
-
-            edge_idx = np.where(graph_masked.adj.edges["in"] == duplo[1])[0]
-            graph_masked.adj.edges["in"][edge_idx] = duplo[0]
-
-    return graph_masked[mask]
+    dups = np.union1d(in_singles[in_dup_idxs], out_singles[out_dup_idxs])
+    # mark these vertices as duplicates
+    mask[dups["index"]] = True
+    return gcl.MaskArray(mask)
