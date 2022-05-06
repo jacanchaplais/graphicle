95e111a95ff198e6c0567372b90d4a208d888d1b
Jacan Chaplais <jacan.chaplais@gmail.com>
2022-05-06 10:59:06 +0100

    removed redundant cdef function #17

--- graphicle/_matrix.pyx
+++ graphicle/_matrix.pyx
@@ -5,9 +5,6 @@ from libc.math cimport hypot
 
 DTYPE = np.double
 
-cdef double delta_R(double eta_1, double eta_2, double phi_1, double phi_2):
-    return hypot(eta_2 - eta_1, phi_2 - phi_1)
-
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def delta_R_aff(double[:] eta, double[:] phi):
@@ -22,18 +19,15 @@ def delta_R_aff(double[:] eta, double[:] phi):
         1d array of azimuthal angles for each particle.
     """
     cdef Py_ssize_t length = eta.shape[0]
-
     assert tuple(eta.shape) == tuple(phi.shape)
 
     result = np.zeros((length, length), dtype=DTYPE)
     cdef double[:, ::1] result_view = result
 
     cdef Py_ssize_t x, y
-
-    # TODO: take advantage of fact matrix is symmetric
     for y in range(1, length):
         for x in range(y):
-            result_view[y, x] = delta_R(eta[y], eta[x], phi[y], phi[x])
+            result_view[y, x] = hypot(eta[y] - eta[x], phi[y] - phi[x])
 
     result = result + result.T
     return result
