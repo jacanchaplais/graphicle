a698bd572fe35e76b061f56becfec92c57df0c30
Jacan Chaplais <jacan.chaplais@gmail.com>
2022-06-06 22:12:39 +0100

    vectorised flow-tracing for structured arrays #28

--- graphicle/calculate.py
+++ graphicle/calculate.py
@@ -5,8 +5,8 @@
 Algorithms for performing common HEP calculations using graphicle data
 structures.
 """
-from typing import Tuple, Optional, Set, List
-from functools import lru_cache
+from typing import Tuple, Optional, Set, List, Dict
+from functools import lru_cache, partial
 
 import numpy as np
 from numpy.lib.recfunctions import (
@@ -50,32 +50,51 @@ def jet_mass(
     return np.sqrt((data.sum(axis=0) ** 2 @ minkowski) + eps)  # type: ignore
 
 
+def _diffuse(colors: List[np.ndarray], feats: List[np.ndarray]):
+    color_shape = colors[0].shape
+    av_color = np.zeros((color_shape[0], color_shape[1]), dtype="<f8")
+    color_stack = np.dstack(colors)  # len_basis x feat_dim x num_in
+    feat_stack = np.vstack(feats).T  # feat_dim x num_in
+    feat_sum = np.sum(feat_stack, axis=1)
+    nonzero_mask = feat_sum != 0.0
+    av_color[:, nonzero_mask] = (
+        np.sum(
+            color_stack[:, nonzero_mask, :] * feat_stack[nonzero_mask], axis=2
+        )
+        / feat_sum[nonzero_mask]
+    )
+    return av_color
+
+
 @lru_cache(maxsize=None)
-def _trace_single(
+def _trace_vector(
     nx_graph: nx.DiGraph,
     vertex: int,
     basis: Tuple[int, ...],
+    feat_dim: int,
+    is_structured: bool,
     exclusive: bool = False,
 ) -> np.ndarray:
-    dtype = "<f8"
+    len_basis = len(basis)
+    feat_fmt = structured_to_unstructured if is_structured else lambda x: x
+    color = np.zeros((len_basis, feat_dim), dtype=_types.double)
     if vertex in basis:
-        color = np.array([base == vertex for base in basis], dtype=dtype)
+        color[basis.index(vertex)] = 1.0
         if exclusive is True:
             return color
-    else:
-        color = np.zeros(len(basis), dtype=dtype)
     in_edges = nx_graph.in_edges(vertex, data=True)
     colors_in: List[np.ndarray] = []
-    color_weights: List[float] = []
+    feats = []
     for edge in in_edges:
-        color_weights.append(edge[2]["weight"])
+        feats.append(feat_fmt(edge[2]["feat"]))
         in_vtx = edge[0]
-        colors_in.append(_trace_single(nx_graph, in_vtx, basis, exclusive))
-    try:
-        color += np.average(colors_in, axis=0, weights=color_weights)
-    except ZeroDivisionError:
-        # this means no incoming colour contribution, so skip adding
-        pass
+        colors_in.append(
+            _trace_vector(
+                nx_graph, in_vtx, basis, feat_dim, is_structured, exclusive
+            )
+        )
+    if colors_in:
+        color += _diffuse(colors_in, feats)
     return color
 
 
@@ -85,7 +104,7 @@ def hard_trace(
     prop: np.ndarray,
     exclusive: bool = False,
     target: Optional[Set[int]] = None,
-) -> np.ndarray:
+) -> Dict[str, np.ndarray]:
     """Performs flow tracing from specified particles in an event, back
     to the hard partons.
 
@@ -119,20 +138,45 @@ def hard_trace(
         traced down to the properties of the selected subset of
         particles specified by mask.
     """
+    # encoding graph features onto NetworkX
     nx_graph = nx.DiGraph()
-    graph_dict = graph.adj.to_dicts(edge_data={"weight": prop})
+    graph_dict = graph.adj.to_dicts(edge_data={"feat": prop})
+    example_feat = graph_dict["edges"][0][2]["feat"]
+    try:
+        feat_dim = len(example_feat)
+        dtype = example_feat.dtype
+    except TypeError:
+        feat_dim = 1
+        dtype = np.dtype(type(example_feat))
+    is_structured = dtype.names is not None
     nx_graph.add_edges_from(graph_dict["edges"])
+    # identify the hard ancestors to which we trace
     hard_mask = graph.hard_mask.copy()
     del hard_mask["incoming"]
     hard_graph = graph[hard_mask.bitwise_or]
-    if target:
+    if target:  # restrict hard partons to user specified pdgs
         target_mask = hard_graph.pdg.mask(
             list(target), blacklist=False, sign_sensitive=True
         )
         hard_graph = hard_graph[target_mask]
     names, vtxs = tuple(hard_graph.pdg.name), tuple(hard_graph.edges["out"])
+    # out vertices of user specified particles
     focus_pcls = graph.edges[mask]["out"]
-    struc_dtype = np.dtype(list(zip(names, ("<f8",) * len(names))))
-    trc = np.array([_trace_single(nx_graph, pcl, vtxs) for pcl in focus_pcls])
-    _trace_single.cache_clear()
-    return unstructured_to_structured(trc, struc_dtype)
+    # struc_dtype = np.dtype(list(zip(names, ("<f8",) * len(names))))
+    trc = np.array(
+        [
+            _trace_vector(nx_graph, pcl, vtxs, feat_dim, is_structured)
+            for pcl in focus_pcls
+        ]
+    )
+    _trace_vector.cache_clear()
+    traces = dict()
+    array_fmt = (
+        partial(unstructured_to_structured, dtype=dtype)
+        if is_structured
+        else lambda x: x.squeeze()
+    )
+
+    for i, name in enumerate(names):
+        traces[name] = array_fmt(trc[:, i, :])
+    return traces
