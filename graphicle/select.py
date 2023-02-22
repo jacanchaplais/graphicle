7833aa84d0925bdce6e51c53a797c4990bda9f91
Jacan Chaplais <jacan.chaplais@gmail.com>
2023-02-22 15:54:13 +0000

    updated matrix property to use protected sparse array #99

--- graphicle/select.py
+++ graphicle/select.py
@@ -213,7 +213,7 @@ def vertex_descendants(adj: gcl.AdjacencyList, vertex: int) -> gcl.MaskArray:
         Boolean mask over the graphicle objects associated with the
         passed AdjacencyList.
     """
-    bft = breadth_first_tree(adj._sparse, abs(vertex))
+    bft = breadth_first_tree(adj._sparse_signed, abs(vertex))
     desc_nodes = (2 * bft.data.astype("<i4") - 1) * bft.indices
     mask = np.isin(adj.edges["in"], desc_nodes)
     mask[adj.edges["in"] == vertex] = True
